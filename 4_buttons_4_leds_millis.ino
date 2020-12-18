8f2925fee4e9a5714b40598610d36f738923fba2
Andrey Krasnokutsky <gef711@gmail.com>
2020-12-19 02:16:18 +0300

    Update 4_buttons_4_leds_millis.ino

--- 4_buttons_4_leds_millis.ino
+++ 4_buttons_4_leds_millis.ino
@@ -1,45 +1,58 @@
-int i=0,freq8=500,freq9=1000,freq10=1000,freq11=400;
-unsigned long t8=millis(),t9=millis(),t10=millis(),t11=millis();
-unsigned long tb2=millis(),tb3=millis(),tb4=millis(),tb5=millis();
+
+int i=0,freq8=1000,freq9=1300,freq10=4100,freq11=5000;//counter i and delays for each LED
+unsigned long t8=millis(),t9=millis(),t10=millis(),t11=millis();//cycle's start times
 void setup()
 {
   Serial.begin(9600);
   randomSeed(analogRead(0));
-  for(i=2;i<=5;i++)//пины ввода с подтягивающими к питанию резисторами
+  for(i=2;i<=5;i++)//input pins (buttons) with pull-up resistors
   {
     pinMode(i,INPUT_PULLUP);
   }
-  for(i=8;i<=11;i++)//пины вывода
+  for(i=8;i<=11;i++)//output pins(LEDs)
   {
     pinMode(i,OUTPUT);
     digitalWrite(i,LOW);
   }
 }
 
-void switchoff()
+void switchoff()//swtich off everything, with timers going on
 {
-  while(digitalRead(3)==HIGH)for(i=8;i<=12;i++)digitalWrite(i,LOW);
-    t8=t9=millis();
-    loop(); 
+  while(digitalRead(2)==HIGH)
+  {
+    for(i=8;i<=11;i++)digitalWrite(i,LOW);//switches off all leds while button is off
+    if(millis()-t8==freq8*2)t8=millis();//variable t continues to renew
+    if(millis()-t9==freq9*2)t9=millis();//as it would be in the normal loop function
+    if(millis()-t10==freq10*2)t10=millis();
+    if(millis()-t11==freq11*2)t11=millis();
+  }
+  loop();//if button is pressed, "while" stops and loop starts
 }
 
-void loop()
+void restart()//restarts all timers
 {
-  if(millis()-t8==0)digitalWrite(8,HIGH);
-  else if(millis()-t8==freq8)digitalWrite(8,LOW);
-  else if(millis()-t8==freq8*2)t8=millis();
+  t8=t9=t10=t11=millis();
+  loop();//and starts loop function
+}
 
-  if(millis()-t9==0)digitalWrite(9,HIGH);
-  else if(millis()-t9==freq9)digitalWrite(9,LOW);
+void loop()//signs < instead of == are made for [switchoff-loop] smooth transition
+{
+  if(millis()-t8<freq8)digitalWrite(8,HIGH);//timers for all buttons
+  else if(millis()-t8<freq8*2)digitalWrite(8,LOW);
+  else if(millis()-t8==freq8*2)t8=millis(); 
+
+  if(millis()-t9<freq9)digitalWrite(9,HIGH);
+  else if(millis()-t9<freq9*2)digitalWrite(9,LOW);
   else if(millis()-t9==freq9*2)t9=millis();
 
-  if(digitalRead(2)==LOW)switchoff();
+  if(millis()-t10<freq10)digitalWrite(10,HIGH);
+  else if(millis()-t10<freq10*2)digitalWrite(10,LOW);
+  else if(millis()-t10==freq10*2)t10=millis();
 
-  //if(millis()-t10==0)digitalWrite(10,HIGH);
-  //else if(millis()-t10==freq10)digitalWrite(10,LOW);
-  //else if(millis()-t10==freq10*2)t10=millis();
+  if(millis()-t11<freq11)digitalWrite(11,HIGH);
+  else if(millis()-t11<freq11*2)digitalWrite(11,LOW);
+  else if(millis()-t11==freq11*2)t11=millis();
 
-  //if(millis()-t11==0)digitalWrite(11,HIGH);
-  //else if(millis()-t11==freq11)digitalWrite(11,LOW);
-  //else if(millis()-t11==freq11*2)t11=millis();
-}
+  if(digitalRead(3)==LOW)switchoff();
+  if(digitalRead(4)==LOW)restart();
+}//button 2 is turning on (from swtich off), button 3 is switch off, button 4 is restart timers;
