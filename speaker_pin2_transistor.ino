f1ae31ee8fc1ecd40243ba0ccf37bff954a3062f
Andrey Krasnokutsky <gef711@gmail.com>
2020-12-21 00:10:55 +0300

    mishka s kukloi, I guess russian song

---
+++ speaker_pin2_transistor.ino
@@ -0,0 +1,86 @@
+#include<pitches.h>
+int i=0;
+int dur1=1000,dur2=500,dur3=1000/3,dur4=250,dur8=125,dur16=62.5;
+int pin=3;
+void setup()
+{
+  pinMode(pin,OUTPUT);
+}
+void loop()
+{
+  tone(pin,NOTE_C4,dur4);delay(dur4);
+  tone(pin,NOTE_D4,dur4);delay(dur4);
+  tone(pin,NOTE_E4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur8);delay(dur8);
+  tone(pin,NOTE_F4,dur8);delay(dur8);
+  tone(pin,NOTE_E4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur8);delay(dur8);
+  tone(pin,NOTE_E4,dur8);delay(dur8);
+  tone(pin,NOTE_D4,dur4);delay(dur4);
+  tone(pin,NOTE_C4,dur4);delay(dur4);
+  tone(pin,NOTE_E4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur2);delay(dur2);
+
+  tone(pin,NOTE_C4,dur4);delay(dur4);
+  tone(pin,NOTE_D4,dur4);delay(dur4);
+  tone(pin,NOTE_E4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur8);delay(dur8);
+  tone(pin,NOTE_F4,dur8);delay(dur8);
+  tone(pin,NOTE_E4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur8);delay(dur8);
+  tone(pin,NOTE_E4,dur8);delay(dur8);
+  tone(pin,NOTE_D4,dur4);delay(dur4);
+  tone(pin,NOTE_C4,dur4);delay(dur4);
+  tone(pin,NOTE_E4,dur4);delay(dur4);
+  tone(pin,NOTE_C4,dur2);delay(dur2);
+
+  tone(pin,NOTE_A4,dur4);delay(dur4);
+  tone(pin,NOTE_A4,dur4);delay(dur4);
+  tone(pin,NOTE_A4,dur8);delay(dur8);
+  tone(pin,NOTE_G4,dur8);delay(dur8);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur8);delay(dur8);
+  tone(pin,NOTE_F4,dur8);delay(dur8);
+  tone(pin,NOTE_E4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur8);delay(dur8);
+  tone(pin,NOTE_E4,dur8);delay(dur8);
+  tone(pin,NOTE_D4,dur4);delay(dur4);
+  tone(pin,NOTE_C4,dur4);delay(dur4);
+  tone(pin,NOTE_E4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur2);delay(dur2);
+
+  tone(pin,NOTE_A4,dur4);delay(dur4);
+  tone(pin,NOTE_A4,dur4);delay(dur4);
+  tone(pin,NOTE_A4,dur8);delay(dur8);
+  tone(pin,NOTE_G4,dur8);delay(dur8);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur4);delay(dur4);
+  tone(pin,NOTE_G4,dur8);delay(dur8);
+  tone(pin,NOTE_F4,dur8);delay(dur8);
+  tone(pin,NOTE_E4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur4);delay(dur4);
+  tone(pin,NOTE_F4,dur8);delay(dur8);
+  tone(pin,NOTE_E4,dur8);delay(dur8);
+  tone(pin,NOTE_D4,dur4);delay(dur4);
+  tone(pin,NOTE_C4,dur4);delay(dur4);
+  tone(pin,NOTE_E4,dur4);delay(dur4);
+  tone(pin,NOTE_C4,dur2);delay(dur2);
+
+  delay(60000);
+}
